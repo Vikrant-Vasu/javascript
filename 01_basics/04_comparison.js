23b5ce0bf8884290bdeb83c9d201fa74f00e8d5c
Vikrant-Vasu <111682145+Vikrant-Vasu@users.noreply.github.com>
2023-11-10 07:10:36 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined >= 0)
+console.log(undefined < 0)
+console.log(undefined <= 0)
+console.log(null == 0)
+console.log(null >= 0)//they both are true
+console.log(null <= 0)//because the value of null gets converted
+                      //in case of comparison operator but in the
+                      //the case of equal to it does not gets converted
+console.log(null < 0)
+console.log(null > 0)
