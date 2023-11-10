bafc9229388327c7198546e9578a240f801111ab
Vikrant-Vasu <111682145+Vikrant-Vasu@users.noreply.github.com>
2023-11-10 13:22:27 +0000

    stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,3 +2,9 @@ const bigNumber = 23847927598783475895265345645645647543765467467567567n
 console.log(typeof bigNumber);
 const id = Symbol('123')
 console.log(typeof id)
+
+
+//********************************************************************//
+// two types of memory
+// stack and heap
+// stack is used in primitive types and heap is used in non- primitive datatypes
