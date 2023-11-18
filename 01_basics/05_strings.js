2ef4bf135ca751dfc9a715df23005b06fca4420b
Vikrant-Vasu <111682145+Vikrant-Vasu@users.noreply.github.com>
2023-11-18 17:43:57 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,14 @@
+const name = "Vikrant";
+const marks = 95;
+
+console.log(name + " "+ marks)
+//better way to add strings is string interpolation
+
+console.log(`My name is ${name} and my marks are ${marks}`);
+
+//we have other method to declare string
+
+const gameName = new String('VikrantVasu')
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName);
