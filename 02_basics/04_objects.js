2d27e50c14fda81196073d7d6eaef3d4700b69cf
Vikrant-Vasu <111682145+Vikrant-Vasu@users.noreply.github.com>
2024-04-26 09:21:50 +0000

    new

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -13,12 +13,13 @@ tinderUser.isLoggedIn = false
 
   const obj1 = {1:"a", 2:"b"}
   const obj2 = {3:"a", 4:"b"}
-  const obj3 = {5:"a", 6:"b"}
+  const obj3 = {4:"a", 6:"b"}
   // we can combine 2 objects bs using assign keyword
 
-//   const obj4 = Object.assign({},obj1,obj2,obj3)
+  // const obj4 = Object.assign({},obj1,obj2,obj3)
      const obj4 = {...obj1, ...obj2, ...obj3}
   // console.log(obj4);
+  // console.log(obj1);
   // console.log(Object.entries(tinderUser))
   // console.log(tinderUser.hasOwnProperty('isLogged'));
   // .hasOwnProperty()
@@ -36,7 +37,7 @@ tinderUser.isLoggedIn = false
 
   //but if we want to print anything to much then we have a syntax
    //                       <> below
-  const{courseInstructor: ci} = course
+  const {courseInstructor: ci} = course
   console.log(ci);
 
   //the JSON look like this below
